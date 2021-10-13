088001effe4034d061b7b1e72658ba20ff6b0810
HowLargeTheTeeth <graham.emily15@gmail.com>
2021-10-13 18:32:02 +1100

    added basic conversion tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import './App.css';
+import { useState } from 'react';
+
+const TextField = ({ value, onChange, placeholder="Enter text here" }) => {
+  const handleChange = (event) => {
+    onChange(event.target.value);
+  }
+
+  return (
+    <input type="text" value={value} onChange={handleChange} /> 
+  );
+}
+
+const Form = () => {
+  const [textState, setTextState] = useState("");
+
+  console.log( textState);
+
+  return (
+    <div>
+      <div>
+        <TextField 
+          value={textState} 
+          onChange={setTextState} 
+          placeholder="What have you got to say?"
+        /> 
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Form /> 
+      {/* <ModalForm /> */}
     </div>
   );
 }
